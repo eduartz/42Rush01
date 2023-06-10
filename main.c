29f46c4c66e5d1238fa128bfa64e4bbd3a82f3e4
Eduartz <eduneoxz@hotmail.com>
2023-06-10 11:54:14 +0200

    add comments

--- main.c
+++ main.c
@@ -2,11 +2,13 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+//solo se usara para imprimir las posiciones despues de hacer la logica
 void ft_print(char c)
 {
 	write(1, &c, 1);
 }
 
+//valida si los datos ingresados existen y si son mayores a 0.
 bool ft_filter(int x, int y)
 {
 	if((x != ' ' && y != ' ') && (x > 0 && y > 0))
@@ -17,6 +19,15 @@ bool ft_filter(int x, int y)
 	}
 }
 
+/*
+	OJO:
+	la logica se puede basar en la complejidad del programa, pero seria mejor crear nuevas funciones
+	que hagan partes pequeñas y su resultado pasar a la logica y que este haga lo que quiera con dicha respuesta.
+	como imprimir en pantalla,  analizar, etc.
+	de tal modo como se hizo con ft_filter.
+*/
+
+//logica del programa
 void ft_logic(int x, int y)
 {
 	if(ft_filter(x, y))
@@ -27,6 +38,7 @@ void ft_logic(int x, int y)
 	}
 }
 
+//ejecucion del programa
 int main ()
 {
 	ft_logic(2, 1);
